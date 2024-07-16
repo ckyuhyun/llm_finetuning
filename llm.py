3012588debe3ea755cb456982d838b0f5dffd8a0
Ted <ckyuhyun@gmail.com>
2024-07-15 21:50:13 -0400

    clean out llm file

--- llm.py
+++ llm.py
@@ -1,159 +1,27 @@
-from datasets import load_dataset, DatasetDict, Dataset
-
-from transformers import (
-    AutoTokenizer,
-    AutoConfig,
-    AutoModelForSequenceClassification,
-    DataCollatorWithPadding,
-    TrainingArguments,
-    Trainer)
-
-from peft import PeftModel, PeftConfig, get_peft_model, LoraConfig
-import evaluate
-import torch
-import numpy as np
+import pandas as pd
 import os
 
-model_checkpoint = 'distilbert-base-uncased'
-
-# define label maps
-id2label = {0: "Negative", 1: "Positive"}
-label2id = {"Negative":0, "Positive":1}
-
-# generate classification model from model_checkpoint
-model = AutoModelForSequenceClassification.from_pretrained(
-    model_checkpoint, num_labels=2, id2label=id2label, label2id=label2id)
-
-# load dataset
-dataset = load_dataset("shawhin/imdb-truncated")
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, add_prefix_space=True)
 
 
 
-# create tokenize function
-def tokenize_function(examples):
-    # extract text
-    text = examples["text"]
+data_src_path = 'qa_datasets'
 
-    #tokenize and truncate text
-    tokenizer.truncation_side = "left"
-    tokenized_inputs = tokenizer(
-        text,
-        return_tensors="np",
-        truncation=True,
-        max_length=512
-    )
+file_path = os.path.join(data_src_path, 'test_qa.csv')
 
-    return tokenized_inputs
+if not os.path.isfile(file_path):
+    raise f'{file_path} is not existed'
 
-# add pad token if none exists
-if tokenizer.pad_token is None:
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    model.resize_token_embeddings(len(tokenizer))
+ds = pd.read_csv(file_path)
+
+if ds.empty:
+    raise f'{file_path} has no data'
+
+valid_columms = ['question', 'review']
+valid_ds = pd.DataFrame(ds, columns=valid_columms)
 
 
-# tokenize training and validation datasets
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
-print(tokenized_dataset)
-
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-# import accuracy evaluation metric
-accuracy = evaluate.load("accuracy")
-
-# define an evaluation function to pass into trainer later
-def compute_metrics(p):
-    predictions, labels = p
-    predictions = np.argmax(predictions, axis=1)
-
-    return {"accuracy": accuracy.compute(predictions=predictions,
-                                          references=labels)}
 
 
-# tokenized_dataset =
-# DatasetDict({
-#     train: Dataset({
-#        features: ['label', 'text', 'input_ids', 'attention_mask'],
-#         num_rows: 1000
-#     })
-#     validation: Dataset({
-#         features: ['label', 'text', 'input_ids', 'attention_mask'],
-#         num_rows: 1000
-#     })
-# })
-
-# define list of examples
-text_list = ["I have no idea", "Not a fan, don't recommed.",
-"Better than the first one.", "This is not worth watching even once.",
-"This one is a pass."]
-
-print("Untrained model predictions:")
-print("----------------------------")
-for text in text_list:
-    # tokenize text
-    inputs = tokenizer.encode(text, return_tensors="pt")
-    # compute logits
-    logits = model(inputs).logits
-    # convert logits to label
-    predictions = torch.argmax(logits)
-
-    print(text + " - " + id2label[predictions.tolist()])
 
 
-peft_config = LoraConfig(task_type="SEQ_CLS",
-                         r=4,
-                         lora_alpha=32,
-                         lora_dropout=0.01,
-                         target_modules=['q_lin'])
-
-model = get_peft_model(model, peft_config)
-
-model.print_trainable_parameters()
-
-# hyperparameters
-lr = 1e-3 # size of optimization step
-batch_size = 4 # number of examples processed per optimziation step
-num_epochs = 10 # number of times model runs through training data
-
-# define training arguments
-training_args = TrainingArguments(
-    output_dir= model_checkpoint + "-lora-text-classification",
-    learning_rate=lr,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    num_train_epochs=num_epochs,
-    weight_decay=0.01,
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
-    load_best_model_at_end=True,
-)
-
-# creater trainer object
-trainer = Trainer(
-    model=model, # our peft model
-    args=training_args, # hyperparameters
-    train_dataset=tokenized_dataset["train"], # training data
-    eval_dataset=tokenized_dataset["validation"], # validation data
-    tokenizer=tokenizer, # define tokenizer
-    data_collator=data_collator, # this will dynamically pad examples in each batch to be equal length
-    compute_metrics=compute_metrics, # evaluates model using compute_metrics() function from before
-)
-
-# train model
-trainer.train()
-
-
-model.to('cpu') # moving to mps for Mac (can alternatively do 'cpu')
-
-print("Trained model predictions:")
-print("--------------------------")
-for text in text_list:
-    inputs = tokenizer.encode(text, return_tensors="pt").to("cpu") # moving to mps for Mac (can alternatively do 'cpu')
-
-    logits = model(inputs).logits
-    predictions = torch.max(logits,1).indices
-
-    print(text + " - " + id2label[predictions.tolist()[0]])
-
-trainer.save_model(os.path.join(os.getcwd(),"trained_model"))
 
