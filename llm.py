b8398c0fd5da50af9737c50ee201c417992a07b2
Ted <ckyuhyun@gmail.com>
2024-07-15 22:47:02 -0400

    init llmpy

--- llm.py
+++ llm.py
@@ -16,8 +16,27 @@ ds = pd.read_csv(file_path)
 if ds.empty:
     raise f'{file_path} has no data'
 
-valid_columms = ['question', 'review']
+valid_columms = ['question','human_ans_indices','review','human_ans_spans']
+remaing_columms = ['question','start_pos','context','answer']
 valid_ds = pd.DataFrame(ds, columns=valid_columms)
+
+
+
+def ds_pre_processing(dataset:pd.DataFrame):
+    input = pd.DataFrame(columns=remaing_columms)
+    for i in range(0, len(dataset.values)):
+        input[i]['question'] = dataset.values[i]['question']
+
+    pass
+
+
+ds_pre_processing(pd.DataFrame(valid_ds, columns=remaing_columms))
+
+
+
+
+
+
 
 
 
