18d5b9fac31c7eedd8636cc17d87230d99152502
Ted <ckyuhyun@gmail.com>
2024-08-27 22:28:39 -0400

    refactorying of llm train model

--- llm.py
+++ llm.py
@@ -10,183 +10,221 @@ from transformers import (
     TrainingArguments,
     Trainer)
 
-data_src_path = 'data_sets'
-trained_model_dic = "trained_model_dic"
 
-file_path = os.path.join(data_src_path, 'LLM_Materials.xlsx')
+class Data_Preprocessing:
+    def __init__(self,
+                 data_src_path,
+                 file_path,
+                 trained_model_dic):
+        self.data_src_path = data_src_path
+        self.file_path = file_path
+        self.trained_model_dic = trained_model_dic
+        self.preprocessed_ds = None
+        self.train_dataset = None
 
-if not os.path.isfile(file_path):
-    raise f'{file_path} is not existed'
+        self.__data_preprocessing()
 
-model_path = "distilbert/distilbert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-
-def update_answer_pos(dataSet: pd.DataFrame) -> pd.DataFrame:
-    reference_context = None
-    new_ds = pd.DataFrame(columns=dataSet.columns)
-
-    for row_data in dataSet.iterrows():
+    def __data_preprocessing(self):
         try:
-            if reference_context is None:
-                reference_context = row_data[1]['context']
+            ds = pd.read_excel(self.file_path)
+        except pd.errors.EmptyDataError:
+            raise f'{self.file_path} is empty'
 
-            reference_context = row_data[1]['context'] if pd.notnull(row_data[1]['context']) and  reference_context != row_data[1]['context'] else reference_context
-        except TypeError as e:
-            raise e
+        if ds.empty:
+            raise f'{self.file_path} has no data'
 
-        expect_answer = row_data[1]['answer'].strip()
-        search_result = re.search(expect_answer, reference_context)  #reference_context.find(expect_answer)   #re.finditer(, row_data[1]['context'])
+        self.preprocessed_ds = self.__cleaning_dataset(ds)
+    def __cleaning_dataset(self, dataSet: pd.DataFrame) -> pd.DataFrame:
+        dataSet.columns = dataSet.columns.str.replace(' ', '')
+        filtered_dataset = dataSet.loc[~dataSet["question"].isna() & ~dataSet["answer"].isna()]
 
-        try:
-            start_pos = search_result.regs[0][0]
-            end_pos = search_result.regs[0][1] #start_pos+len(row_data[1]["answer"])
-            start_end_pos = f'{start_pos},{end_pos}'
-            #ans_pos.append(start_end_pos)
-
-            # new_row_data = { 'category': row_data[1]['category'],
-            #                 'Title': row_data[1]['Title'],
-            #                 'context': row_data[1]['context'],
-            #                 'question': row_data[1]['question'],
-            #                 'answer': row_data[1]['answer'],
-            #                 'answer_pos': start_end_pos,}
-            new_row_data = [row_data[1]['category'],
-                            row_data[1]['Title'],
-                            reference_context,
-                            row_data[1]['question'],
-                            row_data[1]['answer'],
-                            start_end_pos]
-            new_ds.loc[len(new_ds.index)] = new_row_data
+        return filtered_dataset
 
 
+class Training_Model(Data_Preprocessing):
+    def __init__(self,
+                 data_src_path      ='data_sets',
+                 src_file_name      ='LLM_Materials.xlsx',
+                 trained_model_dic  = 'trained_model_dic',
+                 check_point        = "distilbert/distilbert-base-uncased"):
+        self.data_src_path = data_src_path
+        self.file_path = os.path.join(self.data_src_path, src_file_name)
+        self.trained_model_dic = trained_model_dic
+        self.check_point = check_point
+        self.tokenizer = AutoTokenizer.from_pretrained(self.check_point)
+        self.training_args = None
+        self.valid_columms = ['context', 'question', 'answer', 'answer_pos']
 
-        except AttributeError:
-            print(f'answer reference : {row_data[1]["answer"]}')
-            raise Exception('')
-        try:
-            print(f'extracted answer : {reference_context[start_pos: end_pos].strip()}')
-        except Exception as e:
-            raise Exception(f'{reference_context}')
+        super().__init__(self.data_src_path, self.file_path, self.trained_model_dic)
+
+        if not os.path.isfile(self.file_path):
+            raise f'{self.file_path} is not existed'
+
+    def get_context_collection(self):
+        return self.train_dataset['context']
+
+    def run(self,
+            saving_trained_model=False):
+        final_dataSet = self.__update_answer_pos()
+        valid_ds = pd.DataFrame(final_dataSet, columns=self.valid_columms)
+        self.train_dataset = datasets.Dataset.from_pandas(valid_ds)
+
+        token_ds = self.train_dataset.map(self.tokenizing, batched=True, remove_columns=self.train_dataset.column_names)
+        # token_ds = tokenizing(train_dataset['question'],train_dataset['context'], train_dataset['answer_pos'])
+        model = AutoModelForQuestionAnswering.from_pretrained(self.check_point)
+
+        trainer = Trainer(
+            model=model,
+            args=self.training_args,
+            train_dataset=token_ds,
+            # eval_dataset=tokenized_datasets["validation"].select(range(100)),
+            # data_collator=data_collator,
+            tokenizer=self.tokenizer,
+            # train_dataset=small_train_dataset,
+            # eval_dataset=small_eval_dataset,
+            # compute_metrics=compute_metrics,
+        )
+
+        trainer.train()
+
+        if saving_trained_model:
+            if os.path.isdir(self.trained_model_dic):
+                os.mkdir(f'{trainer}')
+            file_name = f"{datetime.now().year}_{datetime.now().month}_{datetime.now().day}{datetime.now().hour}_{datetime.now().minute}_llm_model"
+            trainer.save_model(os.path.join(self.trained_model_dic, file_name))
+
+    def set_tokenizer_configuration(self,
+                                    output_dir=f"fine_llm_result",
+                                    evaluation_strategy="no",
+                                    learning_rate=2e-5,
+                                    per_device_train_batch_size=8,
+                                    per_device_eval_batch_size=8,
+                                    num_train_epochs=2,
+                                    weight_decay=0.01,
+                                    do_eval=False):
+        self.training_args = TrainingArguments(
+            output_dir=output_dir,
+            # evaluation_strategy =evaluation_strategy ,
+            evaluation_strategy=evaluation_strategy,
+            learning_rate=learning_rate,
+            per_device_train_batch_size=per_device_train_batch_size,
+            per_device_eval_batch_size=per_device_eval_batch_size,
+            num_train_epochs=num_train_epochs,
+            weight_decay=weight_decay,
+            do_eval=do_eval
+        )
+
+    def __update_answer_pos(self) -> pd.DataFrame:
+        dataSet = self.preprocessed_ds
+        reference_context = None
+        new_ds = pd.DataFrame(columns=dataSet.columns)
+
+        for row_data in dataSet.iterrows():
+            try:
+                if reference_context is None:
+                    reference_context = row_data[1]['context']
+
+                reference_context = row_data[1]['context'] if pd.notnull(row_data[1]['context']) and reference_context != row_data[1]['context'] else reference_context
+            except TypeError as e:
+                raise e
+
+            expect_answer = row_data[1]['answer'].strip()
+            search_result = re.search(expect_answer, reference_context)  # reference_context.find(expect_answer)   #re.finditer(, row_data[1]['context'])
+
+            try:
+                start_pos = search_result.regs[0][0]
+                end_pos = search_result.regs[0][1]  # start_pos+len(row_data[1]["answer"])
+                start_end_pos = f'{start_pos},{end_pos}'
+                # ans_pos.append(start_end_pos)
+
+                # new_row_data = { 'category': row_data[1]['category'],
+                #                 'Title': row_data[1]['Title'],
+                #                 'context': row_data[1]['context'],
+                #                 'question': row_data[1]['question'],
+                #                 'answer': row_data[1]['answer'],
+                #                 'answer_pos': start_end_pos,}
+                new_row_data = [row_data[1]['category'],
+                                row_data[1]['Title'],
+                                reference_context,
+                                row_data[1]['question'],
+                                row_data[1]['answer'],
+                                start_end_pos]
+                new_ds.loc[len(new_ds.index)] = new_row_data
 
 
-    return new_ds
 
-def cleaning_dataset(dataSet: pd.DataFrame) -> pd.DataFrame:
-    dataSet.columns = dataSet.columns.str.replace(' ', '')
-    filtered_dataset = dataSet.loc[~dataSet["question"].isna() & ~dataSet["answer"].isna() ]
+            except AttributeError:
+                print(f'answer reference : {row_data[1]["answer"]}')
+                raise Exception('')
+            try:
+                print(f'extracted answer : {reference_context[start_pos: end_pos].strip()}')
+            except Exception as e:
+                raise Exception(f'{reference_context}')
 
-    return filtered_dataset
+        return new_ds
 
 
-def tokenizing(examples):
-#def tokenizing(context, question, answer_pos):
-    inputs = tokenizer(examples['question'],
-                      examples['context'],
-                      max_length=384,
-                      padding=True,
-                      truncation=True,
-                      add_special_tokens=True,
-                    return_offsets_mapping=True,
-                      #is_split_into_words=True, # The sequence or batch of sequences to be encoded.
-                                        # Each sequence can be a string or a list of strings (pretokenized string).
-                                        # If the sequences are provided as list of strings (pretokenized),
-                                        # you must set is_split_into_words=True (to lift the ambiguity with a batch of sequences).
-                                )
+    def tokenizing(self, examples):
+        inputs = self.tokenizer(examples['question'],
+                          examples['context'],
+                          max_length=384,
+                          padding=True,
+                          truncation=True,
+                          add_special_tokens=True,
+                          return_offsets_mapping=True,
+                          #is_split_into_words=True, # The sequence or batch of sequences to be encoded.
+                                            # Each sequence can be a string or a list of strings (pretokenized string).
+                                            # If the sequences are provided as list of strings (pretokenized),
+                                            # you must set is_split_into_words=True (to lift the ambiguity with a batch of sequences).
+                                    )
 
-    offset_mapping = inputs.pop('offset_mapping')
-    start_pos = []
-    end_pos = []
-    for i, offset in enumerate(offset_mapping):
-        answer_start_end_position = examples['answer_pos'][i]
-        answer_start = int(re.split(',', answer_start_end_position)[0].strip())
-        answer_end = int(re.split(',', answer_start_end_position)[1].strip())
-        sequence_ids = inputs.sequence_ids(i)
+        offset_mapping = inputs.pop('offset_mapping')
+        start_pos = []
+        end_pos = []
+        for i, offset in enumerate(offset_mapping):
+            answer_start_end_position = examples['answer_pos'][i]
+            answer_start = int(re.split(',', answer_start_end_position)[0].strip())
+            answer_end = int(re.split(',', answer_start_end_position)[1].strip())
+            sequence_ids = inputs.sequence_ids(i)
 
-        index=0
-        while sequence_ids[index] != 1: #question token has '0'
-            index += 1
-        context_start = index
-
-        while sequence_ids[index] == 1: #context token has '1'
-            index += 1
-        context_end = index - 1
-
-        # If the answer is not fully inside the context, label it (0, 0)
-        if offset[context_start][0] > answer_end or offset[context_end][1] < answer_start :
-            start_pos.append(0)
-            end_pos.append(0)
-        else:
-            index = context_start
-            while index < context_end and offset[index][0] <= answer_start:
+            index=0
+            while sequence_ids[index] != 1: #question token has '0'
                 index += 1
-            start_pos.append(index-1)
+            context_start = index
 
-            while index >= context_start and offset[index][1] >= answer_end:
-                index -= 1
-            end_pos.append(index+1)
+            while sequence_ids[index] == 1: #context token has '1'
+                index += 1
+            context_end = index - 1
 
+            # If the answer is not fully inside the context, label it (0, 0)
+            if offset[context_start][0] > answer_end or offset[context_end][1] < answer_start :
+                start_pos.append(0)
+                end_pos.append(0)
+            else:
+                index = context_start
+                while index < context_end and offset[index][0] <= answer_start:
+                    index += 1
+                start_pos.append(index-1)
 
-
-    inputs["start_positions"] = start_pos
-    inputs["end_positions"] = end_pos
-
-    return inputs
-
-try:
-    ds = pd.read_excel(file_path)
-except pd.errors.EmptyDataError:
-    raise f'{file_path} is empty'
-
-if ds.empty:
-    raise f'{file_path} has no data'
-
-cleaned_dataset = cleaning_dataset(ds)
-final_dataSet = update_answer_pos(cleaned_dataset)
-
-valid_columms = ['context', 'question', 'answer', 'answer_pos']
-#remaing_columms = ['question', 'start_pos', 'context', 'answer']
-valid_ds = pd.DataFrame(final_dataSet, columns=valid_columms)
-
-train_dataset = datasets.Dataset.from_pandas(valid_ds)
-
-token_ds = train_dataset.map(tokenizing, batched=True, remove_columns=train_dataset.column_names)
-#token_ds = tokenizing(train_dataset['question'],train_dataset['context'], train_dataset['answer_pos'])
-model = AutoModelForQuestionAnswering.from_pretrained(model_path)
+                while index >= context_start and offset[index][1] >= answer_end:
+                    index -= 1
+                end_pos.append(index+1)
 
 
 
-training_args = TrainingArguments(
-    output_dir=f"fine_llm_result",
-    #evaluation_strategy = "epoch",
-    evaluation_strategy = "no",
-    learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=2,
-    weight_decay=0.01,
-    do_eval=False
-)
+        inputs["start_positions"] = start_pos
+        inputs["end_positions"] = end_pos
+
+        return inputs
 
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset= token_ds,
-    #eval_dataset=tokenized_datasets["validation"].select(range(100)),
-    #data_collator=data_collator,
-    tokenizer=tokenizer,
-    #train_dataset=small_train_dataset,
-    #eval_dataset=small_eval_dataset,
-    #compute_metrics=compute_metrics,
-)
 
-trainer.train()
 
-if os.path.isdir(trained_model_dic):
-    os.mkdir(f'{trainer}')
-file_name = f"{datetime.now().year}_{datetime.now().month}_{datetime.now().day}{datetime.now().hour}_{datetime.now().minute}_llm_model"
-trainer.save_model(os.path.join(trained_model_dic, file_name))
 
-pass
+
+
+
+
 
 
 
--- llm_runner.py
+++ llm_runner.py
@@ -3,15 +3,22 @@ import os.path
 from transformers import (
                     pipeline)
 
+from llm import Data_Preprocessing, Training_Model
+
+model = Training_Model()
+model.run()
 
 trained_model_dic = "trained_model_dic"
 model_name = '2024_8_2518_12_llm_model'
 pipe = pipeline("question-answering",
                 model=os.path.join(trained_model_dic, model_name))
 
-context = "Alert can be submitted approval or rejection for updating of the data on the Alerts &  Approvals widget. Once opening a panel for a project on the Project Forms or a product family on the Reusable Content or Reusable Records tab page, there are a check button in green and a x button in red. The check button is for an approval, and the x button is for an rejection. if any button among the check button or x button got selected by clicking, the submit button would appear, and get highlighted the value that would be remained on the spot at the end. If the alert item in the Project Forms is submitted for an apporval, the project form page would be updated with the updated value that has been changed from the reusable content page or reusable records page by administrator. If the alert item in the Reusable Content is submitted for an approval, the all of project forms pages and the reusable records page that have the reusable content data would be updated with the updated value. . If the alert item in the Reusable Record is submitted for an approval, the all of project forms pages that have the reusable record data would be updated with the updated value. In addition, if the alert item for the reusable content data in the Reusable Records is submitted, it would ask if updating the reusable content data in the reusable content page on the Alert Submit modal. If you want to update the resuable content data as well along with updating for the reusable record data, make the checkbox checked and click the Confirm button on the Alert Submit modal. Once submitting the alert item, it would be removed from the widget"
+#context = "Alert can be submitted approval or rejection for updating of the data on the Alerts &  Approvals widget. Once opening a panel for a project on the Project Forms or a product family on the Reusable Content or Reusable Records tab page, there are a check button in green and a x button in red. The check button is for an approval, and the x button is for an rejection. if any button among the check button or x button got selected by clicking, the submit button would appear, and get highlighted the value that would be remained on the spot at the end. If the alert item in the Project Forms is submitted for an apporval, the project form page would be updated with the updated value that has been changed from the reusable content page or reusable records page by administrator. If the alert item in the Reusable Content is submitted for an approval, the all of project forms pages and the reusable records page that have the reusable content data would be updated with the updated value. . If the alert item in the Reusable Record is submitted for an approval, the all of project forms pages that have the reusable record data would be updated with the updated value. In addition, if the alert item for the reusable content data in the Reusable Records is submitted, it would ask if updating the reusable content data in the reusable content page on the Alert Submit modal. If you want to update the resuable content data as well along with updating for the reusable record data, make the checkbox checked and click the Confirm button on the Alert Submit modal. Once submitting the alert item, it would be removed from the widget"
 
 
-result = pipe(question="how to get the reusable content alert?", context=context)
-print(f'answer : {result}')
+context_collection = model.get_context_collection()
+
+pass
+# result = pipe(question="how to get the reusable content alert?", context=context)
+# print(f'answer : {result}')
 
